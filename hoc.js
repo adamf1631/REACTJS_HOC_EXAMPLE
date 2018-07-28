0c3d48e8589108f0c55913829db6df75c0f5e0d2
adamf1631 <adamf1631@yahoo.com>
2018-07-28 12:13:03 -0400

    Initial commit

---
+++ hoc.js
@@ -0,0 +1,45 @@
+//higher order component A component that renders another component
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+  <div>
+    <h1>
+      info
+    </h1>
+    <p>
+      the info is : {props.info}
+    </p>
+  </div>
+);
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+    { props.isAdmin && <p>This is private info, please dont fuck</p>}
+      <WrappedComponent {...props} />
+    </div>
+  );
+
+};
+
+const requireAuthen = (WrappedComponent) => {
+  return (props) => (
+    <div>
+    { props.isAuthenticated ? (
+      <WrappedComponent {...props} />
+      ) : (
+        <p>Please log in to see info</p>
+      )}
+
+    </div>
+  );
+
+};
+
+
+const AdminInfo = withAdminWarning(Info);
+//const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthen(Info);
+
+//ReactDOM.render(<AdminInfo isAdmin={true} info="These are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="These are the details" />, document.getElementById('app'));
